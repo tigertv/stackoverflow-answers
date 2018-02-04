116d169da64272e42e720f4aa1812633624dd562
tigertv <maxvetrov555@yandex.ru>
2018-02-04 16:48:43 +0200

    a new question

---
+++ 48604474/main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -A astr
+
+while IFS=, read -r col1 col2
+do
+	if [ "$col1" != "SKU" ] && [ "$col1" != "" ]
+	then
+		(( astr[$col1] = col2 ))
+	fi
+done < 1a.csv 
+
+while IFS=, read -r col1 col2
+do
+	if [ "$col1" != "SKU" ] && [ "$col1" != "" ]
+	then
+		(( astr[$col1] = col2 ))
+	fi
+done < 2a.csv 
+
+echo "SKU,QTY"
+for i in "${!astr[@]}"
+do   
+	echo "$i,${astr[$i]}"
+done | sort -t : -k 2n

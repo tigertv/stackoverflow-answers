387e89556fe76a7fd688cf5d4da92d40d6600c13
tigertv <maxvetrov555@yandex.ru>
2018-02-05 19:40:50 +0200

    only awk

--- 48604474/awk.py
+++ 48604474/awk.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
 import os, sys
 
-print "SKU,QTY"
-os.system ("awk -F, 'NR>1{a[$1] = $2}END{for (i in a) if(i != \"SKU\") print i\",\"a[i]}' 1a.csv 2a.csv")
+os.system ("awk -F, 'BEGIN{print \"SKU,QTY\"}NR>1{a[$1] = $2}END{for (i in a) if(i != \"SKU\") print i\",\"a[i]}' 1a.csv 2a.csv")
--- 48604474/awk.sh
+++ 48604474/awk.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-echo "SKU,QTY"
-awk -F, 'NR>1{a[$1] = $2}END{for (i in a) if(i != "SKU")print i","a[i]}' 1a.csv 2a.csv
+awk -F, 'BEGIN{print "SKU,QTY"}NR>1{a[$1] = $2}END{for (i in a) if(i != "SKU")print i","a[i]}' 1a.csv 2a.csv

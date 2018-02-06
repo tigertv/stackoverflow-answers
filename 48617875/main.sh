d29e20eb1c6677474e89ec03aef481535c4e022e
tigertv <maxvetrov555@yandex.ru>
2018-02-06 15:04:11 +0200

    filter by each time interval

--- 48617875/main.sh
+++ 48617875/main.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-awk -F: '$3!=p{print ""}{p=$3}{print}' sys.log
+Year='$1'
+Month='$2$3'
+Day='$2$3$4'
+Hour='$2$3$4$5'
+Minute='$2$3$4$5$6'
+Second='$2$3$4$5$6$7'
+
+filter=$Minute
+
+awk -F'[ :-]' $filter'!=p{print ""}{p='$filter'}{print}' sys.log

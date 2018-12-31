662205f3e6e2b7d265b3b5c7891b6aa353552c90
tigertv <maxvetrov555@yandex.ru>
2018-12-31 03:05:38 +0200

    Add base for numbers

--- 8194141-how-to-print-a-number-in-assembly-nasm/print.s
+++ 8194141-how-to-print-a-number-in-assembly-nasm/print.s
@@ -4,7 +4,8 @@ global _start
 section .text
 _start:
 
-mov eax, 123456
+mov eax, 123 ; unsigned number to print
+mov ebx, 12  ; base to represent the number, do not set it too big
 call print
 
 ;exit
@@ -15,20 +16,32 @@ int 0x80
 print:
 mov edi, 1
 mov ecx, esp
-mov ebx, 10
+add esp, 12
 dec ecx
-mov [ecx], bl
+mov edx, 10
+mov [ecx], dl
 
 print_loop:
 
 xor edx, edx
-idiv ebx
+div ebx
+cmp dl, 9     ; if reminder>9 go to use_letter
+jg use_letter
+
 add dl, '0'
+jmp after_use_letter
+
+use_letter:
+add dl, 'W'   ; letters from 'a' to ... in ascii code
+
+after_use_letter:
 dec ecx
 inc edi
 mov [ecx],dl
 test eax, eax
 jnz print_loop
+
+sub esp, 12
 
 mov eax, 4
 mov ebx, 1
